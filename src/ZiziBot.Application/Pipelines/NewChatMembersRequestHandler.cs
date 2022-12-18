203a370f7f6ccc578a297eb8b3d1dee5c00b20ef
Azhe Kun <azhe403@gmail.com>
2022-12-18 22:43:38 +0700

    add basic welcome message

---
+++ src/ZiziBot.Application/Pipelines/NewChatMembersRequestHandler.cs
@@ -0,0 +1,43 @@
+﻿using JetBrains.Annotations;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace ZiziBot.Application.Pipelines;
+
+public class NewChatMembersRequestModel : RequestBase
+{
+}
+
+[UsedImplicitly]
+internal class NewChatMembersRequestHandler : IRequestHandler<NewChatMembersRequestModel, ResponseBase>
+{
+    private readonly IMediator _mediator;
+    private readonly ILogger<NewChatMembersRequestHandler> _logger;
+
+    public NewChatMembersRequestHandler(IMediator mediator, ILogger<NewChatMembersRequestHandler> logger)
+    {
+        _mediator = mediator;
+        _logger = logger;
+    }
+
+    public async Task<ResponseBase> Handle(NewChatMembersRequestModel request, CancellationToken cancellationToken)
+    {
+        ResponseBase responseBase = new();
+        _logger.LogInformation("New Chat Members. ChatId: {ChatId}", request.Message.Chat.Id);
+
+        var message = "Hai " + request.Message.From.FirstName + " " + request.Message.From.LastName + " selamat datang di grup ";
+
+        responseBase.Complete();
+
+        _mediator.Enqueue(new SendMessageTextRequestModel()
+        {
+            Message = request.Message,
+            BotData = request.BotData,
+            ReplyToMessageId = request.Message.MessageId,
+            DeleteAfter = TimeSpan.FromMinutes(1),
+            Text = message
+        });
+
+        return responseBase;
+    }
+}
---
+++ src/ZiziBot.Engine/Controllers/MemberChangesController.cs
@@ -0,0 +1,27 @@
+﻿using Allowed.Telegram.Bot.Attributes;
+using Allowed.Telegram.Bot.Controllers;
+using Allowed.Telegram.Bot.Models;
+using MediatR;
+using Telegram.Bot.Types.Enums;
+
+namespace ZiziBot.Engine.Controllers;
+
+public class MemberChangesController : CommandController
+{
+    private readonly IMediator _mediator;
+
+    public MemberChangesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [TypedCommand(MessageType.ChatMembersAdded)]
+    public void NewChatMembers(MessageData data)
+    {
+        _mediator.Enqueue(new NewChatMembersRequestModel()
+        {
+            BotData = data.BotData,
+            Message = data.Message
+        });
+    }
+}
