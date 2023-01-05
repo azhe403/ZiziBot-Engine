6087adaf7a9415cb489073a5ec2e9c3254e9bbcc
Azhe Kun <azhe403@gmail.com>
2023-01-05 17:07:58 +0700

    add MongoDbContextBase.cs

--- src/ZiziBot.DataSource/MongoDb/AppSettingsDbContext.cs
+++ src/ZiziBot.DataSource/MongoDb/AppSettingsDbContext.cs
@@ -1,37 +1,12 @@
 using MongoFramework;
-using MongoFramework.Infrastructure;
 
 namespace ZiziBot.DataSource.MongoDb;
 
-public class AppSettingsDbContext : MongoDbContext
+public class AppSettingsDbContext : MongoDbContextBase
 {
-    public AppSettingsDbContext(string connectionStr) : base(MongoDbConnection.FromConnectionString(connectionStr))
-    {
-    }
+	public AppSettingsDbContext(string connectionStr) : base(connectionStr) {}
 
-    public MongoDbSet<AppSettings> AppSettings { get; set; }
-    public MongoDbSet<BotSettings> BotSettings { get; set; }
-    public MongoDbSet<Sudoer> Sudoers { get; set; }
-
-    public override Task SaveChangesAsync(CancellationToken cancellationToken = new())
-    {
-        var entries = ChangeTracker
-            .Entries()
-            .Where(e =>
-                e.Entity is EntityBase &&
-                e.State is EntityEntryState.Added or EntityEntryState.Updated
-            );
-
-        foreach (var entityEntry in entries)
-        {
-            ((EntityBase)entityEntry.Entity).UpdateTime = DateTime.Now;
-
-            if (entityEntry.State == EntityEntryState.Added)
-            {
-                ((EntityBase)entityEntry.Entity).CreatedDate = DateTime.Now;
-            }
-        }
-
-        return base.SaveChangesAsync(cancellationToken);
-    }
+	public MongoDbSet<AppSettings> AppSettings { get; set; }
+	public MongoDbSet<BotSettings> BotSettings { get; set; }
+	public MongoDbSet<Sudoer> Sudoers { get; set; }
 }
---
+++ src/ZiziBot.DataSource/MongoDb/MongoDbContextBase.cs
@@ -0,0 +1,33 @@
+using MongoFramework;
+using MongoFramework.Infrastructure;
+
+namespace ZiziBot.DataSource.MongoDb;
+
+public class MongoDbContextBase : MongoDbContext
+{
+	public MongoDbContextBase(string connectionStr) : base(MongoDbConnection.FromConnectionString(connectionStr)) {}
+
+	public override Task SaveChangesAsync(CancellationToken cancellationToken = new())
+	{
+		var entries = ChangeTracker
+			.Entries()
+			.Where(
+				e =>
+					e.Entity is EntityBase &&
+					e.State is EntityEntryState.Added or EntityEntryState.Updated
+			);
+
+		foreach (var entityEntry in entries)
+		{
+			((EntityBase) entityEntry.Entity).UpdateTime = DateTime.Now;
+
+			if (entityEntry.State == EntityEntryState.Added)
+			{
+				((EntityBase) entityEntry.Entity).CreatedDate = DateTime.Now;
+			}
+		}
+
+		return base.SaveChangesAsync(cancellationToken);
+	}
+
+}
