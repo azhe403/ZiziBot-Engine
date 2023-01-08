6f8b9ca3e4240a4ac80668cc5766fce17163f0a7
Azhe Kun <azhe403@gmail.com>
2023-01-08 20:20:23 +0700

    remove dashboard loader

--- src/ZiziBot.Engine/GlobalUsings.cs
+++ src/ZiziBot.Engine/GlobalUsings.cs
@@ -4,6 +4,5 @@ global using ZiziBot.Allowed.TelegramBot;
 global using ZiziBot.Contracts.Constants;
 global using ZiziBot.Hangfire;
 global using ZiziBot.Infrastructure;
-global using ZiziBot.NgDashboard;
 global using ZiziBot.Utils;
 global using ZiziBot.WebApi.Providers;
