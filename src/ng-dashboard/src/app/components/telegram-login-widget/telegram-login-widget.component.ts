84f388b3139abd24e2397825d5254e749cb1eacd
Azhe Kun <azhe403@gmail.com>
2023-01-09 21:32:15 +0700

    setup staging deployment

--- src/ng-dashboard/src/app/components/telegram-login-widget/telegram-login-widget.component.ts
+++ src/ng-dashboard/src/app/components/telegram-login-widget/telegram-login-widget.component.ts
@@ -1,15 +1,31 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import {TelegramUserLogin} from "../../types/TelegramUserLogin";
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'app-telegram-login-widget',
   templateUrl: './telegram-login-widget.component.html',
   styleUrls: ['./telegram-login-widget.component.scss']
 })
-export class TelegramLoginWidgetComponent {
+export class TelegramLoginWidgetComponent implements AfterViewInit{
+  botName = environment.botName;
+
+  constructor() {
+
+  }
 
   onTelegramAuth(user: TelegramUserLogin) {
     alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+  }
+
+  ngAfterViewInit(): void {
+    this.loadWidgetLogin();
+  }
+
+  loadWidgetLogin(){
+    const loginWidget = document.getElementById('telegram-login-widget');
+    // @ts-ignore
+    loginWidget.setAttribute('data-telegram-login', this.botName);
   }
 
 }
---
+++ src/ng-dashboard/src/environments/environment.staging.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  botName: 'MissZiziBeta5Bot'
+};
