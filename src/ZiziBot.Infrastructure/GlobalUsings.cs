cff4817a08b1a4d9289161ea24bc2469c6c864fc
Azhe Kun <azhe403@gmail.com>
2023-01-21 14:19:01 +0700

    fix build

--- src/ZiziBot.Infrastructure/GlobalUsings.cs
+++ src/ZiziBot.Infrastructure/GlobalUsings.cs
@@ -1,5 +1,6 @@
 // Global using directives
 
+global using ZiziBot.Application.Handlers.Telegram.Basic;
 global using ZiziBot.Application.Handlers.Telegram.Core;
 global using ZiziBot.Application.Services;
 global using ZiziBot.Caching;
