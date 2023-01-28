e3c5906aeb57c32c3bae4700a4f9c6f674cfe7e7
Azhe Kun <azhe403@gmail.com>
2023-01-28 15:14:20 +0700

    remember drawer state

--- src/ng-dashboard/src/app/root/root.component.ts
+++ src/ng-dashboard/src/app/root/root.component.ts
@@ -1,17 +1,20 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {DashboardService} from "../services/dashboard/dashboard.service";
-import {Router} from "@angular/router";
+import {NavigationEnd, Router} from "@angular/router";
 import {StorageService} from '../services/storage/storage.service';
+import {StorageKey} from '../consts/storage-key';
 
 @Component({
   selector: 'app-root',
   templateUrl: './root.component.html',
   styleUrls: ['./root.component.scss']
 })
-export class RootComponent {
+export class RootComponent implements OnInit, AfterViewInit {
 
   sessionId: string | undefined;
   menus: any = [];
+
+  @ViewChild('drawer') drawer: any;
 
   constructor(
     private router: Router,
@@ -19,6 +22,18 @@ export class RootComponent {
     private dashboardService: DashboardService
   ) {
     this.buildMenu();
+  }
+
+  ngAfterViewInit(): void {
+    this.loadDrawerState();
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        this.buildMenu();
+      }
+    })
   }
 
   onLogout() {
@@ -38,9 +53,29 @@ export class RootComponent {
         url: '/mirror-user/management',
       },
       {
+        title: 'Notes',
+        url: '/notes',
+      },
+      {
         title: 'Angular',
         url: '/angular',
       }
     ];
+  }
+
+  toggleDrawer() {
+    console.debug('toggleDrawer');
+
+    this.storageService.set(StorageKey.DRAWER_STATE, this.drawer.opened ? 'false' : 'true');
+    this.drawer.toggle();
+  }
+
+  loadDrawerState() {
+    const drawer = this.storageService.get(StorageKey.DRAWER_STATE);
+    if (drawer == 'true') {
+      this.drawer.open();
+    } else {
+      this.drawer.close();
+    }
   }
 }
