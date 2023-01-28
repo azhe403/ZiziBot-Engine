c48298e604fef8a6662344bda9c1967cc2e3803b
Azhe Kun <azhe403@gmail.com>
2023-01-28 19:28:11 +0700

    set attempts

--- src/ZiziBot.Hangfire/MediatorBridge.cs
+++ src/ZiziBot.Hangfire/MediatorBridge.cs
@@ -12,14 +12,14 @@ public class MediatorBridge
     }
 
     [DisplayName("{0}")]
-    [AutomaticRetry(OnAttemptsExceeded = AttemptsExceededAction.Delete)]
+    [AutomaticRetry(OnAttemptsExceeded = AttemptsExceededAction.Delete, Attempts = 3)]
     public async Task<object?> Send(IBaseRequest command)
     {
         return await _mediator.Send(command);
     }
 
     [DisplayName("{0}")]
-    [AutomaticRetry(OnAttemptsExceeded = AttemptsExceededAction.Delete)]
+    [AutomaticRetry(OnAttemptsExceeded = AttemptsExceededAction.Delete, Attempts = 3)]
     public async Task<object?> Send(string jobName, IBaseRequest command)
     {
         return await _mediator.Send(command);
