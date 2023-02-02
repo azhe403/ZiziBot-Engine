bd7998d15088671f6271714b43300a8a9754f32b
Azhe Kun <azhe403@gmail.com>
2023-02-02 18:37:50 +0700

    add fluent validation util

---
+++ src/ZiziBot.Utils/FValidationUtil.cs
@@ -0,0 +1,23 @@
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace ZiziBot.Utils;
+
+public static class FValidationUtil
+{
+    public static ValidationResult? Validate<TValidator, TObj>(this TObj toValidate) where TValidator : class, new() where TObj : class
+    {
+        var validatorInstance = new TValidator() as AbstractValidator<TObj>;
+        var results = validatorInstance?.Validate(toValidate);
+
+        return results;
+    }
+
+    public static async Task<ValidationResult?> ValidateAsync<TValidator, TObj>(this TObj toValidate) where TValidator : class, new() where TObj : class
+    {
+        var validatorInstance = new TValidator() as AbstractValidator<TObj>;
+        var results = await validatorInstance?.ValidateAsync(toValidate)!;
+
+        return results;
+    }
+}
