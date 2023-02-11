732231f1ae64758c64c3cbcb629850174b2dfc16
Azhe Kun <azhe403@gmail.com>
2023-02-11 18:21:41 +0700

    return transactionId in json

--- src/ZiziBot.Application/Core/ApiResponseBase.cs
+++ src/ZiziBot.Application/Core/ApiResponseBase.cs
@@ -5,6 +5,7 @@ namespace ZiziBot.Application.Core;
 public class ApiResponseBase<T>
 {
     public HttpStatusCode StatusCode { get; set; }
+    public string transactionId { get; set; }
     public string Message { get; set; }
     public T Result { get; set; }
 }
