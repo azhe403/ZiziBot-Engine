14b76eac0834a88c079f160c369ebca113467009
Azhe Kun <azhe403@gmail.com>
2023-02-19 13:28:26 +0700

    improve note list

--- src/ZiziBot.Application/Handlers/Telegram/Note/GetNoteRequest.cs
+++ src/ZiziBot.Application/Handlers/Telegram/Note/GetNoteRequest.cs
@@ -6,18 +6,19 @@ public class GetNoteRequestModel : RequestBase
 
 public class GetNoteRequestHandler : IRequestHandler<GetNoteRequestModel, ResponseBase>
 {
+    private readonly TelegramService _telegramService;
     private readonly NoteService _noteService;
 
-    public GetNoteRequestHandler(NoteService noteService)
+    public GetNoteRequestHandler(TelegramService telegramService, NoteService noteService)
     {
+        _telegramService = telegramService;
         _noteService = noteService;
     }
 
     public async Task<ResponseBase> Handle(GetNoteRequestModel request, CancellationToken cancellationToken)
     {
-        ResponseBase responseBase = new(request);
-
         var htmlMessage = HtmlMessage.Empty;
+        _telegramService.SetupResponse(request);
 
         var tags = await _noteService.GetAllByChat(request.ChatIdentifier);
 
@@ -27,10 +28,23 @@ public class GetNoteRequestHandler : IRequestHandler<GetNoteRequestModel, Respon
         }
         else
         {
-            htmlMessage.Bold("Daftar Catatan:").Br();
-            tags.ForEach(entity => htmlMessage.Text(entity.Query).Br());
+            htmlMessage.Bold("Daftar Tagar:").Br();
+            var grouped = tags
+                .GroupBy(entity => entity.Query.Split(" ").Length <= 1)
+                .ToList();
+
+            grouped.First(x => x.Key == true)
+                .ToList()
+                .ForEach(entity => htmlMessage.Text("#").Text(entity.Query).Text(" "));
+
+            htmlMessage.Br().Br()
+                .Bold("Daftar Catatan:").Br();
+
+            grouped.First(x => x.Key == false)
+                .ToList()
+                .ForEach(entity => htmlMessage.Text("- ").Code(entity.Query).Br());
         }
 
-        return await responseBase.SendMessageText(htmlMessage.ToString());
+        return await _telegramService.SendMessageText(htmlMessage.ToString());
     }
 }
