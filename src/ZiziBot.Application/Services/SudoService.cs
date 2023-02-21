64d27bc6ca3be855d79b11e57f4fb81cf0cfeeef
Azhe Kun <azhe403@gmail.com>
2023-02-22 00:48:51 +0700

    caching check sudo

--- src/ZiziBot.Application/Services/SudoService.cs
+++ src/ZiziBot.Application/Services/SudoService.cs
@@ -4,37 +4,46 @@ namespace ZiziBot.Application.Services;
 
 public class SudoService
 {
-	private readonly AppSettingsDbContext _appSettingsDbContext;
+    private readonly AppSettingsDbContext _appSettingsDbContext;
+    private readonly CacheService _cacheService;
 
-	public SudoService(AppSettingsDbContext appSettingsDbContext)
-	{
-		_appSettingsDbContext = appSettingsDbContext;
-	}
+    public SudoService(AppSettingsDbContext appSettingsDbContext, CacheService cacheService)
+    {
+        _appSettingsDbContext = appSettingsDbContext;
+        _cacheService = cacheService;
+    }
 
-	public async Task<bool> IsSudoAsync(long userId)
-	{
-		return await _appSettingsDbContext.Sudoers.AnyAsync(
-			x =>
-				x.UserId == userId &&
-				x.Status == (int) EventStatus.Complete
-		);
-	}
+    public async Task<bool> IsSudoAsync(long userId)
+    {
+        var cache = await _cacheService.GetOrSetAsync(
+            cacheKey: CacheKey.SUDO + userId,
+            action: async () => {
+                return await _appSettingsDbContext.Sudoers.AnyAsync(
+                    x =>
+                        x.UserId == userId &&
+                        x.Status == (int) EventStatus.Complete
+                );
+            }
+        );
 
-	public async Task<ServiceResult> SaveSudo(Sudoer sudoer)
-	{
-		ServiceResult serviceResult = new();
+        return cache;
+    }
 
-		var findSudo = await _appSettingsDbContext.Sudoers
-			.FirstOrDefaultAsync(x => x.UserId == sudoer.UserId);
+    public async Task<ServiceResult> SaveSudo(Sudoer sudoer)
+    {
+        ServiceResult serviceResult = new();
 
-		if (findSudo != null)
-		{
-			return serviceResult.Complete("This user is already a sudoer.");
-		}
+        var findSudo = await _appSettingsDbContext.Sudoers
+            .FirstOrDefaultAsync(x => x.UserId == sudoer.UserId);
 
-		_appSettingsDbContext.Sudoers.Add(sudoer);
-		await _appSettingsDbContext.SaveChangesAsync();
+        if (findSudo != null)
+        {
+            return serviceResult.Complete("This user is already a sudoer.");
+        }
 
-		return serviceResult.Complete("Sudoer added successfully.");
-	}
+        _appSettingsDbContext.Sudoers.Add(sudoer);
+        await _appSettingsDbContext.SaveChangesAsync();
+
+        return serviceResult.Complete("Sudoer added successfully.");
+    }
 }
