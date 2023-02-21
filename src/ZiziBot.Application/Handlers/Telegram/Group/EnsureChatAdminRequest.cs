e663c4fbaf9863bd2941dde75b9da4fe99f2c5b0
Azhe Kun <azhe403@gmail.com>
2023-02-22 00:34:28 +0700

    fix post-processor behavior

--- src/ZiziBot.Application/Handlers/Telegram/Group/EnsureChatAdminRequest.cs
+++ src/ZiziBot.Application/Handlers/Telegram/Group/EnsureChatAdminRequest.cs
@@ -1,24 +1,9 @@
-using FluentValidation;
+using MediatR.Pipeline;
 using Microsoft.Extensions.Logging;
 
 namespace ZiziBot.Application.Handlers.Telegram.Group;
 
-public class EnsureChatAdminRequestModel : RequestBase
-{
-    public long ChatId { get; set; }
-    public long UserId { get; set; }
-}
-
-public class EnsureChatAdminRequestModelValidator : AbstractValidator<EnsureChatAdminRequestModel>
-{
-    public EnsureChatAdminRequestModelValidator()
-    {
-        RuleFor(x => x.ChatId).LessThan(0);
-        RuleFor(x => x.UserId).GreaterThan(0);
-    }
-}
-
-public class EnsureChatAdminRequestHandler : IRequestHandler<EnsureChatAdminRequestModel, ResponseBase>
+public class EnsureChatAdminRequestHandler : IRequestPostProcessor<RequestBase, ResponseBase>
 {
     private readonly ILogger<EnsureChatAdminRequestHandler> _logger;
     private readonly TelegramService _telegramService;
@@ -32,16 +17,9 @@ public class EnsureChatAdminRequestHandler : IRequestHandler<EnsureChatAdminRequ
 
     }
 
-    public async Task<ResponseBase> Handle(EnsureChatAdminRequestModel request, CancellationToken cancellationToken)
+    public async Task Process(RequestBase request, ResponseBase response, CancellationToken cancellationToken)
     {
         _telegramService.SetupResponse(request);
-        var validate = await request.ValidateAsync<EnsureChatAdminRequestModelValidator, EnsureChatAdminRequestModel>();
-
-        if (!(validate?.IsValid ?? false))
-        {
-            _logger.LogDebug("Ensure Chat Admin. ChatId: {ChatId}, UserId: {UserId}. Payload invalid", request.ChatId, request.UserId);
-            return _telegramService.Complete();
-        }
 
         _chatDbContext.ChatAdmin
             .RemoveRange(
@@ -51,14 +29,14 @@ public class EnsureChatAdminRequestHandler : IRequestHandler<EnsureChatAdminRequ
 
         await _chatDbContext.SaveChangesAsync(cancellationToken);
 
-        var chatAdministrators = await _telegramService.GetChatAdministrator(request.ChatId);
+        var chatAdministrators = await _telegramService.GetChatAdministrator(request.ChatIdentifier);
         _logger.LogDebug("List of Administrator in ChatId: {ChatId} found {ChatAdministrators} item(s)", request.ChatId, chatAdministrators.Length);
 
         var chatAdminEntities = chatAdministrators
             .Select(
                 x => new ChatAdminEntity()
                 {
-                    ChatId = request.ChatId,
+                    ChatId = request.ChatIdentifier,
                     UserId = x.User.Id,
                     Role = x.Status,
                     Status = (int) EventStatus.Complete
@@ -69,7 +47,5 @@ public class EnsureChatAdminRequestHandler : IRequestHandler<EnsureChatAdminRequ
         _chatDbContext.ChatAdmin.AddRange(chatAdminEntities);
 
         await _chatDbContext.SaveChangesAsync(cancellationToken);
-
-        return _telegramService.Complete();
     }
 }
