926e0c052c44718b176f6888d49701caa686ba33
Azhe Kun <azhe403@gmail.com>
2023-02-27 18:33:02 +0700

    apply minimum role

--- src/ZiziBot.Allowed.TelegramBot/Controllers/NoteController.cs
+++ src/ZiziBot.Allowed.TelegramBot/Controllers/NoteController.cs
@@ -18,8 +18,7 @@ public class NotesController : CommandController
     [Command("notes")]
     public async Task GetNotes(MessageData data)
     {
-        await _mediatorService.EnqueueAsync(
-            new GetNoteRequestModel()
+        await _mediatorService.EnqueueAsync(new GetNoteRequestModel()
             {
                 BotToken = data.Options.Token,
                 Message = data.Message,
@@ -38,10 +37,10 @@ public class NotesController : CommandController
     [Command("renote")]
     public async Task CreateNote(MessageData data)
     {
-        await _mediatorService.EnqueueAsync(
-            new CreateNoteRequestModel()
+        await _mediatorService.EnqueueAsync(new CreateNoteRequestModel()
             {
                 BotToken = data.Options.Token,
+                MinimumRole = RoleLevel.ChatAdminOrPrivate,
                 Message = data.Message,
                 ReplyMessage = true,
                 Query = data.Params.GetCommandParamAt<string>(0, separator: "\n"),
