9d98bf2817951f41b284e324273fedcdd84c1852
Azhe Kun <azhe403@gmail.com>
2023-02-27 18:46:22 +0700

    add DataType property

--- src/ZiziBot.Allowed.TelegramBot/Controllers/NoteController.cs
+++ src/ZiziBot.Allowed.TelegramBot/Controllers/NoteController.cs
@@ -19,18 +19,17 @@ public class NotesController : CommandController
     public async Task GetNotes(MessageData data)
     {
         await _mediatorService.EnqueueAsync(new GetNoteRequestModel()
+        {
+            BotToken = data.Options.Token,
+            Message = data.Message,
+            ReplyMessage = true,
+            DeleteAfter = TimeSpan.FromHours(1),
+            CleanupTargets = new[]
             {
-                BotToken = data.Options.Token,
-                Message = data.Message,
-                ReplyMessage = true,
-                DeleteAfter = TimeSpan.FromHours(1),
-                CleanupTargets = new[]
-                {
-                    CleanupTarget.FromBot,
-                    CleanupTarget.FromSender
-                }
+                CleanupTarget.FromBot,
+                CleanupTarget.FromSender
             }
-        );
+        });
     }
 
     [Command("note")]
@@ -38,22 +37,22 @@ public class NotesController : CommandController
     public async Task CreateNote(MessageData data)
     {
         await _mediatorService.EnqueueAsync(new CreateNoteRequestModel()
+        {
+            BotToken = data.Options.Token,
+            MinimumRole = RoleLevel.ChatAdminOrPrivate,
+            Message = data.Message,
+            ReplyMessage = true,
+            Query = data.Params.GetCommandParamAt<string>(0, separator: "\n"),
+            Content = data.Message.ReplyToMessage?.Text,
+            RawButton = data.Params.GetCommandParamAt<string>(1, separator: "\n"),
+            FileId = data.Message.ReplyToMessage?.GetFileId(),
+            DataType = (int) data.Message.ReplyToMessage.Type,
+            RefreshNote = data.Message.Text?.StartsWith("/renote"),
+            CleanupTargets = new[]
             {
-                BotToken = data.Options.Token,
-                MinimumRole = RoleLevel.ChatAdminOrPrivate,
-                Message = data.Message,
-                ReplyMessage = true,
-                Query = data.Params.GetCommandParamAt<string>(0, separator: "\n"),
-                Content = data.Message.ReplyToMessage?.Text,
-                RawButton = data.Params.GetCommandParamAt<string>(1, separator: "\n"),
-                FileId = data.Message.ReplyToMessage?.GetFileId(),
-                RefreshNote = data.Message.Text?.StartsWith("/renote"),
-                CleanupTargets = new[]
-                {
-                    CleanupTarget.FromBot,
-                    CleanupTarget.FromSender
-                }
+                CleanupTarget.FromBot,
+                CleanupTarget.FromSender
             }
-        );
+        });
     }
 }
