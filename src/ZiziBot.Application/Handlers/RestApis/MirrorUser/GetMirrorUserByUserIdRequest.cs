6dcb8c670188c8eafdff33e9667074ed5d9e6e0b
Azhe Kun <azhe403@gmail.com>
2023-03-17 17:24:17 +0700

    update check mirror user subscription

--- src/ZiziBot.Application/Handlers/RestApis/MirrorUser/GetMirrorUserByUserIdRequest.cs
+++ src/ZiziBot.Application/Handlers/RestApis/MirrorUser/GetMirrorUserByUserIdRequest.cs
@@ -11,9 +11,9 @@ public class GetMirrorUserByUserIdRequestDto : ApiRequestBase<GetMirrorUserDto>
 public class GetMirrorUserDto
 {
     public long UserId { get; set; }
-    public bool IsExpired { get; set; }
-    public DateTime SubscriptionUntil { get; set; }
-    public DateTime MemberSince { get; set; }
+    public bool HasSubscription { get; set; }
+    public DateTime? SubscriptionUntil { get; set; }
+    public DateTime? MemberSince { get; set; }
 }
 
 public class GetMirrorUserByUserIdRequestHandler : IRequestHandler<GetMirrorUserByUserIdRequestDto, ApiResponseBase<GetMirrorUserDto>>
@@ -30,29 +30,32 @@ public class GetMirrorUserByUserIdRequestHandler : IRequestHandler<GetMirrorUser
         ApiResponseBase<GetMirrorUserDto> response = new();
 
         var user = await _mirrorDbContext.MirrorUsers
-            .Where(x => x.UserId == request.UserId)
+            .Where(x =>
+                x.UserId == request.UserId &&
+                x.Status == (int)EventStatus.Complete
+            )
             .FirstOrDefaultAsync(cancellationToken: cancellationToken);
 
         if (user == null)
         {
             response.StatusCode = HttpStatusCode.NotFound;
             response.Message = "User not found";
+
             return response;
         }
         else
         {
             response.StatusCode = HttpStatusCode.OK;
             response.Message = "User found";
+            response.Result = new GetMirrorUserDto
+            {
+                UserId = request.UserId,
+                HasSubscription = user.ExpireDate > DateTime.UtcNow,
+                SubscriptionUntil = user.ExpireDate,
+                MemberSince = user.CreatedDate
+            };
+
+            return response;
         }
-
-        response.Result = new GetMirrorUserDto
-        {
-            UserId = user.UserId,
-            IsExpired = user.ExpireDate < DateTime.UtcNow,
-            SubscriptionUntil = user.ExpireDate,
-            MemberSince = user.CreatedDate
-        };
-
-        return response;
     }
 }
