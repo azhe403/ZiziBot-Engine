a6bb4b0d2f4b26b750aaefb42b2320b3eaad4616
Azhe Kun <azhe403@gmail.com>
2023-03-17 22:20:25 +0700

    init webhook support

--- src/ZiziBot.WebApi/GlobalUsings.cs
+++ src/ZiziBot.WebApi/GlobalUsings.cs
@@ -4,4 +4,6 @@ global using ZiziBot.Application.Core;
 global using ZiziBot.Application.Handlers.RestApis.DashboardSession;
 global using ZiziBot.Application.Handlers.RestApis.GlobalBan;
 global using ZiziBot.Application.Handlers.RestApis.MirrorUser;
+global using ZiziBot.Application.Services;
 global using ZiziBot.WebApi.Middlewares;
+global using ZiziBot.WebApi.Providers;
