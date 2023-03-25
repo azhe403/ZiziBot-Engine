8e00d21059a3824ac28a3e0eaf339b6ea5a2f481
Azhe Kun <azhe403@gmail.com>
2023-03-25 19:05:47 +0700

    add field

--- src/ZiziBot.DataSource/MongoDb/Entities/RssSettingEntity.cs
+++ src/ZiziBot.DataSource/MongoDb/Entities/RssSettingEntity.cs
@@ -8,5 +8,6 @@ public class RssSettingEntity : EntityBase
     public long ChatId { get; set; }
     public long UserId { get; set; }
     public string RssUrl { get; set; }
+    public string CronJobId { get; set; }
     public string LastErrorMessage { get; set; }
 }
