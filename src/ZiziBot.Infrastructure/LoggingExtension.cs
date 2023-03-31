e17670317598e5d77e46e6b4d5d487ef58f37479
Azhe Kun <azhe403@gmail.com>
2023-03-31 10:19:23 +0700

    core: update logging configuration

--- src/ZiziBot.Infrastructure/LoggingExtension.cs
+++ src/ZiziBot.Infrastructure/LoggingExtension.cs
@@ -12,23 +12,25 @@ public static class LoggingExtension
     private const string TEMPLATE_BASE = $"[{{Level:u3}}]{{MemoryUsage}}{{ThreadId}} {{Message:lj}}{{NewLine}}{{Exception}}";
     private const string OUTPUT_TEMPLATE = $"{{Timestamp:HH:mm:ss.fff}} {TEMPLATE_BASE}";
 
-    public static IHostBuilder InitSerilogBootstrapper(this IHostBuilder hostBuilder)
+    public static IHostBuilder InitSerilogBootstrapper(this IHostBuilder hostBuilder, bool fullMode = false)
     {
         hostBuilder.UseSerilog((context, provider, config) => {
+            config.ReadFrom.Configuration(context.Configuration)
+                .ReadFrom.Services(provider)
+                .MinimumLevel.Debug()
+                .Enrich.WithDemystifiedStackTraces();
+
+            config.WriteTo.Async(configuration => configuration.Console(outputTemplate: OUTPUT_TEMPLATE));
+
+            if (!fullMode)
+                return;
+
             var appDbContext = provider.GetRequiredService<AppSettingsDbContext>();
 
             var chatId = appDbContext.AppSettings.FirstOrDefault(entity => entity.Name == "EventLog:ChatId")?.Value;
             var botToken = appDbContext.BotSettings.FirstOrDefault(entity => entity.Name == "Main")?.Token;
 
-            config
-                .ReadFrom.Configuration(context.Configuration)
-                .ReadFrom.Services(provider)
-                .MinimumLevel.Debug()
-                .Enrich.WithDemystifiedStackTraces()
-                .WriteTo.Async(configuration => configuration
-                    .Console(outputTemplate: OUTPUT_TEMPLATE)
-                    .Telegram(botToken, chatId.ToInt64())
-                );
+            config.WriteTo.Async(configuration => configuration.Telegram(botToken, chatId.ToInt64()));
         });
 
         return hostBuilder;
