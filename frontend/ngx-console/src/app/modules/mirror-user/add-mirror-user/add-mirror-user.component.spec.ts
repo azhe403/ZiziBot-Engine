d81c482fbafeb9757e16cf81fdfdbc194944b5ba
Azhe Kun <azhe403@gmail.com>
2023-04-02 23:48:18 +0700

    adjust frontend project directory

---
+++ frontend/ngx-console/src/app/modules/mirror-user/add-mirror-user/add-mirror-user.component.spec.ts
@@ -0,0 +1,23 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {AddMirrorUserComponent} from './add-mirror-user.component';
+
+describe('AddMirrorUserComponent', () => {
+  let component: AddMirrorUserComponent;
+  let fixture: ComponentFixture<AddMirrorUserComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AddMirrorUserComponent]
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(AddMirrorUserComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
