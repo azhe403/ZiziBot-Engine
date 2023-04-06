45415e54527f2eef7c4c89ed83a93050098e0516
Azhe-kun <azhe403@gmail.com>
2023-04-06 16:10:34 +0700

    disable chart module

--- frontend/zizibot-console/src/app/demo/components/uikit/uikit-routing.module.ts
+++ frontend/zizibot-console/src/app/demo/components/uikit/uikit-routing.module.ts
@@ -4,7 +4,7 @@ import {RouterModule} from '@angular/router';
 @NgModule({
     imports: [RouterModule.forChild([
         {path: 'button', data: {breadcrumb: 'Button'}, loadChildren: () => import('./button/buttondemo.module').then(m => m.ButtonDemoModule)},
-        {path: 'charts', data: {breadcrumb: 'Charts'}, loadChildren: () => import('./charts/chartsdemo.module').then(m => m.ChartsDemoModule)},
+        // {path: 'charts', data: {breadcrumb: 'Charts'}, loadChildren: () => import('./charts/chartsdemo.module').then(m => m.ChartsDemoModule)},
         {path: 'file', data: {breadcrumb: 'File'}, loadChildren: () => import('./file/filedemo.module').then(m => m.FileDemoModule)},
         {path: 'floatlabel', data: {breadcrumb: 'Float Label'}, loadChildren: () => import('./floatlabel/floatlabeldemo.module').then(m => m.FloatlabelDemoModule)},
         {path: 'formlayout', data: {breadcrumb: 'Form Layout'}, loadChildren: () => import('./formlayout/formlayoutdemo.module').then(m => m.FormLayoutDemoModule)},
