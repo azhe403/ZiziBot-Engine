68e9f253763abc1b080ad4c647f53be0dc3e2e55
Azhe-kun <azhe403@gmail.com>
2023-04-28 06:42:06 +0700

    test: fix firebase post

---
+++ backend/ZiziBot.Contracts/Configs/GcpConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace ZiziBot.Contracts.Configs;
+
+[DisplayName("Gcp")]
+public class GcpConfig
+{
+    public string FirebaseProjectUrl { get; set; }
+    public string FirebaseServiceAccountJson { get; set; }
+}
