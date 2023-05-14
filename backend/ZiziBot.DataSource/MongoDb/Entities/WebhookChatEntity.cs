5bb0457b508c71b0a054791b146c6ed7e60d4549
Azhe-kun <azhe403@gmail.com>
2023-05-14 15:08:56 +0700

    core: update entity

--- backend/ZiziBot.DataSource/MongoDb/Entities/WebhookChatEntity.cs
+++ backend/ZiziBot.DataSource/MongoDb/Entities/WebhookChatEntity.cs
@@ -3,6 +3,7 @@ namespace ZiziBot.DataSource.MongoDb.Entities;
 [Table("WebhookChat")]
 public class WebhookChatEntity : EntityBase
 {
-    public long ChatId { get; set; }
     public string RouteId { get; set; }
+    public long ChatId { get; set; }
+    public int MessageThreadId { get; set; }
 }
