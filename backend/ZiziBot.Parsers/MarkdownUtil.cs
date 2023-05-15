b7e606bfd1b293b9fa64f4cd5c248bd427a6f8fd
Azhe-kun <azhe403@gmail.com>
2023-05-16 05:54:55 +0700

    api: convert md to html

---
+++ backend/ZiziBot.Parsers/MarkdownUtil.cs
@@ -0,0 +1,12 @@
+﻿using Markdig;
+
+namespace ZiziBot.Parsers;
+
+public static class MarkdownUtil
+{
+    public static string MdToHtml(this string markdown)
+    {
+        var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+        return Markdown.ToHtml(markdown, pipeline);
+    }
+}
