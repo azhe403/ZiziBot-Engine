d43fca8f73da5af1e819cb049bd94d266378c4a2
Azhe-kun <azhe403@gmail.com>
2023-05-22 06:00:16 +0700

    web: add authGuard

--- frontend/zizibot-console/src/app/features/components/administrator/log-viewer/log-viewer-routing.module.ts
+++ frontend/zizibot-console/src/app/features/components/administrator/log-viewer/log-viewer-routing.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LogViewerComponent} from "./log-viewer/log-viewer.component";
+import {authGuard} from "../../../../core/auth.guard";
 
 const routes: Routes = [
-    {path: '', component: LogViewerComponent}
+    {path: '', component: LogViewerComponent, canActivate: [authGuard], data: {role: 'Sudo'}}
 ];
 
 @NgModule({
