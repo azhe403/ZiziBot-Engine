ea939ed7aee03cbd06ec6bad3a02db5c38983a0f
Azhe-kun <azhe403@gmail.com>
2023-05-22 22:42:01 +0700

    web: filter log

--- frontend/zizibot-console/src/app/features/components/administrator/log-viewer/log-viewer.module.ts
+++ frontend/zizibot-console/src/app/features/components/administrator/log-viewer/log-viewer.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { LogViewerRoutingModule } from './log-viewer-routing.module';
-import { LogViewerComponent } from './log-viewer/log-viewer.component';
+import {LogViewerRoutingModule} from './log-viewer-routing.module';
+import {LogViewerComponent} from './log-viewer/log-viewer.component';
 import {TableModule} from "primeng/table";
+import {DropdownModule} from "primeng/dropdown";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import {TableModule} from "primeng/table";
     imports: [
         CommonModule,
         LogViewerRoutingModule,
-        TableModule
+        TableModule,
+        DropdownModule,
+        FormsModule
     ]
 })
 export class LogViewerModule { }
