7383e4b219dd1a5c95133ce8a3c11ca49e1077ca
Azhe Kun <azhe403@gmail.com>
2023-07-04 20:56:56 +0700

    web: fix using

--- backend/ZiziBot.Console/GlobalUsings.cs
+++ backend/ZiziBot.Console/GlobalUsings.cs
@@ -1,7 +1,6 @@
 ﻿// Global using directives
 global using ZiziBot.Application.Handlers.Web.Console;
 global using ZiziBot.Console.Extensions;
-global using ZiziBot.Console.Partials;
 global using ZiziBot.Contracts.Dtos;
 global using ZiziBot.DataSource.MongoDb;
 global using ZiziBot.DataSource.Repository;
