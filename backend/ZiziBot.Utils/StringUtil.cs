0c3a864dd139aee047f03fad5661358f4ce1bff7
Azhe Kun <azhe403@gmail.com>
2023-09-27 21:24:50 +0700

    fix rawButton when create note

--- backend/ZiziBot.Utils/StringUtil.cs
+++ backend/ZiziBot.Utils/StringUtil.cs
@@ -121,4 +121,41 @@ public static class StringUtil
     {
         return string.Join(separator, source);
     }
+
+    public static string TrimStart(this string source, string? value, StringComparison comparisonType = default)
+    {
+        if (source == null)
+            throw new ArgumentNullException(nameof(source));
+
+        if (string.IsNullOrEmpty(value))
+            return source;
+
+        int valueLength = value.Length;
+        int startIndex = 0;
+        while (source.IndexOf(value, startIndex, comparisonType) == startIndex)
+        {
+            startIndex += valueLength;
+        }
+
+        return source.Substring(startIndex);
+    }
+
+    public static string TrimEnd(this string source, string? value, StringComparison comparisonType = default)
+    {
+        if (source == null)
+            throw new ArgumentNullException(nameof(source));
+
+        if (string.IsNullOrEmpty(value))
+            return source;
+
+        int sourceLength = source.Length;
+        int valueLength = value.Length;
+        int count = sourceLength;
+        while (source.LastIndexOf(value, count, comparisonType) == count - valueLength)
+        {
+            count -= valueLength;
+        }
+
+        return source.Substring(0, count);
+    }
 }
