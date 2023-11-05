116d6f233331613e0ecc9ab3ad34f8b3ae3dfc06
Azhe Kun <azhe403@gmail.com>
2023-11-05 15:31:10 +0700

    add auth attribute

--- backend/ZiziBot.Console/ViewModels/BotUserViewModel.cs
+++ backend/ZiziBot.Console/ViewModels/BotUserViewModel.cs
@@ -1,12 +1,27 @@
-﻿using ZiziBot.DataSource.MongoDb.Entities;
+﻿using MongoFramework.Linq;
+using ZiziBot.Contracts.Enums;
+using ZiziBot.DataSource.MongoDb.Entities;
 
 namespace ZiziBot.Console.ViewModels;
 
 public class BotUserViewModel : ReactiveObject, IActivatableViewModel
 {
+    private readonly MongoDbContextBase _mongoDbContextBase;
     public ViewModelActivator Activator { get; }
 
     [Reactive]
-    private IEnumerable<BotUserEntity> BotUser { get; set; }
+    public IEnumerable<BotUserEntity> BotUsers { get; private set; }
 
+    public BotUserViewModel(MongoDbContextBase mongoDbContextBase)
+    {
+        _mongoDbContextBase = mongoDbContextBase;
+
+    }
+
+    public async Task GetUsers()
+    {
+        BotUsers = await _mongoDbContextBase.BotUser.AsNoTracking()
+                                            .Where(x => x.Status == (int)EventStatus.Complete)
+                                            .ToListAsync();
+    }
 }
--- backend/ZiziBot.Contracts/Constants/ValueConst.cs
+++ backend/ZiziBot.Contracts/Constants/ValueConst.cs
@@ -6,4 +6,15 @@ namespace ZiziBot.Contracts.Constants;
 public static class ValueConst
 {
     public const string DEFAULT_WELCOME_MESSAGE = "Hai {users}\nSelamat datang di Kontrakan {request.ChatTitle}";
+
+    public static readonly IEnumerable<int> PaginationSize = new[]
+    {
+        10,
+        20,
+        50,
+        100,
+        200,
+        500,
+        1000
+    };
 }
