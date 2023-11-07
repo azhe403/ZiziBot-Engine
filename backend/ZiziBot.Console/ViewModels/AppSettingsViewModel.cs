1367824425d56249e6e564fb42248c0363c411af
Azhe Kun <azhe403@gmail.com>
2023-11-07 23:14:48 +0700

    ui app settings, bot users, etc

---
+++ backend/ZiziBot.Console/ViewModels/AppSettingsViewModel.cs
@@ -0,0 +1,34 @@
+﻿using MongoFramework.Linq;
+using ZiziBot.Contracts.Enums;
+using ZiziBot.DataSource.MongoDb.Entities;
+
+namespace ZiziBot.Console.ViewModels;
+
+public class AppSettingsViewModel : ReactiveObject, IActivatableViewModel
+{
+    private readonly MongoDbContextBase _mongoDbContextBase;
+    public ViewModelActivator? Activator { get; }
+
+    [Reactive]
+    public IEnumerable<AppSettingsEntity>? AppSettings { get; set; }
+
+    public AppSettingsViewModel(MongoDbContextBase mongoDbContextBase)
+    {
+        _mongoDbContextBase = mongoDbContextBase;
+    }
+
+    public async Task LoadData()
+    {
+        AppSettings = await _mongoDbContextBase.AppSettings.AsNoTracking()
+            .Where(w => w.Status == (int)EventStatus.Complete)
+            .OrderBy(o => o.Field).ToListAsync();
+    }
+
+    public async Task Update(AppSettingsEntity appSettingsEntity)
+    {
+        _mongoDbContextBase.AppSettings.Update(appSettingsEntity);
+        await _mongoDbContextBase.SaveChangesAsync();
+
+        await LoadData();
+    }
+}
