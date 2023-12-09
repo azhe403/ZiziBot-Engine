97e544a86200cf9bc611086650d1c9adef7be5d4
Azhe Kun <azhe403@gmail.com>
2023-12-09 09:13:46 +0700

    scrap cities

---
+++ backend/ZiziBot.Parsers/WebParser/JadwalSholatOrgParserUtil.cs
@@ -0,0 +1,29 @@
+﻿using AngleSharp.Html.Dom;
+using ZiziBot.Types.Vendor.JadwalSholatOrg;
+
+namespace ZiziBot.Parsers.WebParser;
+
+public static class JadwalSholatOrgParserUtil
+{
+    private const string WebUrl = "https://www.jadwalsholat.org/adzan/monthly.php";
+
+    public static async Task<IEnumerable<City>?> GetCities()
+    {
+        var document = await WebUrl.OpenUrl();
+
+        var cities = document?.QuerySelectorAll("option")
+            .Cast<IHtmlOptionElement>()
+            .Select(x => new City()
+            {
+                CityId = x.Value.Convert<int>(),
+                CityCode = x.Text.ToLower().RegexReplace("[^a-zA-Z]", ""),
+                CityName = x.Text
+            });
+
+        return cities;
+    }
+
+    public static void GetMonthly()
+    {
+    }
+}
---
+++ backend/ZiziBot.Tests/Features/JadwalSholatOrgParserTests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace ZiziBot.Tests.Features;
+
+public class JadwalSholatOrgParserTests
+{
+    [Fact]
+    public async Task GetCitiesTest()
+    {
+        var cities = await JadwalSholatOrgParserUtil.GetCities();
+
+        cities.Should().NotBeNullOrEmpty();
+    }
+}
