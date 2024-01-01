5056a5ab7e5b7b1f37e3a4fef289c875d6196714
Azhe Kun <azhe403@gmail.com>
2024-01-02 06:27:20 +0700

    pin message command

---
+++ backend/ZiziBot.Application/Handlers/Telegram/Chat/PinMessageRequest.cs
@@ -0,0 +1,31 @@
+﻿namespace ZiziBot.Application.Handlers.Telegram.Chat;
+
+public class PinMessageRequest : BotRequestBase
+{
+}
+
+public class PinMessageHandler : IBotRequestHandler<PinMessageRequest>
+{
+    private readonly TelegramService _telegramService;
+
+    public PinMessageHandler(TelegramService telegramService)
+    {
+        _telegramService = telegramService;
+    }
+
+    public async Task<BotResponseBase> Handle(PinMessageRequest request, CancellationToken cancellationToken)
+    {
+        _telegramService.SetupResponse(request);
+
+        if (request.ReplyToMessage == null)
+        {
+            return await _telegramService.SendMessageAsync("Spesifikan pesan yang ingin dipin..");
+        }
+
+        await _telegramService.SendMessageAsync("📍 Sedang mengepin pesan..");
+
+        await _telegramService.PinChatMessageAsync(request.ReplyToMessage.MessageId);
+
+        return await _telegramService.SendMessageAsync("📌 Pesan berhasil dipin");
+    }
+}
