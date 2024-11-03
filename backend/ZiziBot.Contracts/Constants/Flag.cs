4bedc413aaff63dceca02e12fc402fd341812632
Azhe Kun <root@corp.azhe.my.id>
2024-11-03 08:24:19 +0700

    featureFlag seeder

--- backend/ZiziBot.Contracts/Constants/Flag.cs
+++ backend/ZiziBot.Contracts/Constants/Flag.cs
@@ -3,24 +3,33 @@ using System.Reflection;
 using ZiziBot.Contracts.Dtos;
 
 // ReSharper disable InconsistentNaming
-
 namespace ZiziBot.Contracts.Constants;
 
-public class Flag
+public static class Flag
 {
-    [DefaultValue(true)] public static string CONSOLE = "CONSOLE";
+    [DefaultValue(true)]
+    public const string CONSOLE = "CONSOLE";
 
-    [DefaultValue(true)] public static string HANGFIRE = "HANGFIRE";
+    [DefaultValue(true)]
+    public const string HANGFIRE = "HANGFIRE";
 
-    [DefaultValue(true)] public static string RSS_BROADCASTER = "RSS_BROADCASTER";
+    [DefaultValue(true)]
+    public const string RSS_BROADCASTER = "RSS_BROADCASTER";
 
-    [DefaultValue(false)] public static string SALAT_TIME = "SALAT_TIME";
+    [DefaultValue(false)]
+    public const string SALAT_TIME = "SALAT_TIME";
 
-    [DefaultValue(false)] public static string ANTI_RAID = "ANTI_RAID";
+    [DefaultValue(false)]
+    public const string ANTI_RAID = "ANTI_RAID";
 
-    [DefaultValue(false)] public static string RSS_INCLUDE_CONTENT = "RSS_INCLUDE_CONTENT";
+    [DefaultValue(false)]
+    public const string RSS_INCLUDE_CONTENT = "RSS_INCLUDE_CONTENT";
 
-    [DefaultValue(true)] public static string RSS_RESET_AT_STARTUP = "RSS_RESET_AT_STARTUP";
+    [DefaultValue(true)]
+    public const string RSS_RESET_AT_STARTUP = "RSS_RESET_AT_STARTUP";
+
+    [DefaultValue(true)]
+    public const string COMMAND_PING = "COMMAND_PING";
 
     public static List<FlagDto> GetFields()
     {
