58bff258c1f6c9c0a78da2591d424df1378825f3
Azhe Kun <root@corp.azhe.my.id>
2024-11-11 00:05:26 +0700

    update fe (2)

---
+++ frontend/libs/zizibot-contracts/src/restapi/user-login.ts
@@ -0,0 +1,15 @@
+export interface TelegramUserLogin {
+  id: string;
+  first_name?: string;
+  last_name?: string;
+  username?: string;
+  photo_url?: string;
+  auth_date: string;
+  hash: string;
+  session_id: string;
+}
+
+export interface TelegramUserLoginResponse {
+  isSessionValid: boolean;
+  bearerToken: string;
+}
---
+++ frontend/libs/zizibot-rest-service/src/internal/user-rest.ts
@@ -0,0 +1,11 @@
+﻿import apiClient from '../utils/http-client';
+import { ApiResponse } from 'zizibot-contracts/src/restapi/api-response';
+import { TelegramUserLogin, TelegramUserLoginResponse } from 'zizibot-contracts/src/restapi/user-login';
+
+export async function validateTelegramSession(data: TelegramUserLogin) {
+  const response = await apiClient.post<ApiResponse<TelegramUserLoginResponse>>('/api/user/session/telegram', data);
+
+  console.debug('auth telegram', response);
+
+  return response.data;
+}
