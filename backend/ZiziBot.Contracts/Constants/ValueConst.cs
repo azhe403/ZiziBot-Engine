969601dbeca39c698bffeffc8d0354577098fe98
Azhe Kun <root@corp.azhe.my.id>
2024-12-07 06:18:31 +0700

    unique key

--- backend/ZiziBot.Contracts/Constants/ValueConst.cs
+++ backend/ZiziBot.Contracts/Constants/ValueConst.cs
@@ -8,17 +8,19 @@ public static class ValueConst
     public const ExecutionStrategy DEFAULT_EXECUTION_STRATEGY = ExecutionStrategy.Instant;
 
     public const int NEW_MEMBER_RAID_WINDOW_LIMIT = 7;
-    public static readonly TimeSpan NEW_MEMBER_RAID_SLIDING_WINDOW = TimeSpan.FromMinutes(5);
-    public static readonly TimeSpan NEW_MEMBER_RAID_MODE_MUTE_DURATION = TimeSpan.FromHours(1);
+    public readonly static TimeSpan NEW_MEMBER_RAID_SLIDING_WINDOW = TimeSpan.FromMinutes(5);
+    public readonly static TimeSpan NEW_MEMBER_RAID_MODE_MUTE_DURATION = TimeSpan.FromHours(1);
 
-    public static readonly TimeSpan NEW_MEMBER_ENTRY_SAFE_LIMIT = TimeSpan.FromDays(3);
+    public readonly static TimeSpan NEW_MEMBER_ENTRY_SAFE_LIMIT = TimeSpan.FromDays(3);
 
-    public static readonly char[] WordSeparator = [' ', '\n', ':', ';', ','];
+    public readonly static char[] WordSeparator = [' ', '\n', ':', ';', ','];
+
+    public static string UNIQUE_KEY = "";
 
     public const string NEW_MEMBER_DEFAULT_WELCOME_MESSAGE = "Hai {users}" +
                                                              "\nSelamat datang di Kontrakan <b>{ChatTitle}</b>";
 
-    public static readonly IEnumerable<int> PaginationSize = new[] {
+    public readonly static IEnumerable<int> PaginationSize = new[] {
         10,
         20,
         50,
