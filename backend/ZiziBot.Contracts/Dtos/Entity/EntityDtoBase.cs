0b5573cda5ba1a3fb899b5cfe00060a9ce38fd89
Azhe Kun <root@corp.azhe.my.id>
2025-06-09 07:48:51 +0700

    scheduled create activity

--- backend/ZiziBot.Contracts/Dtos/Entity/EntityDtoBase.cs
+++ backend/ZiziBot.Contracts/Dtos/Entity/EntityDtoBase.cs
@@ -5,5 +5,7 @@ public class EntityDtoBase
     public int Status { get; set; }
     public string? TransactionId { get; set; }
     public DateTime CreatedDate { get; set; }
+    public long? CreatedBy { get; set; }
     public DateTime UpdatedDate { get; set; }
+    public long? UpdatedBy { get; set; }
 }
