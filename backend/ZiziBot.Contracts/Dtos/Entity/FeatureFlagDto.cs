a8a88afc0464778983ff44648e5c3b15ddb1fc17
Azhe Kun <root@corp.azhe.my.id>
2025-06-22 01:07:36 +0700

    update contracts

---
+++ backend/ZiziBot.Contracts/Dtos/Entity/FeatureFlagDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ZiziBot.Contracts.Dtos.Entity;
+
+public class FeatureFlagDto : EntityDtoBase
+{
+    public required string Id { get; set; }
+    public required string Name { get; set; }
+    public bool IsEnabled { get; set; }
+}
--- backend/ZiziBot.Contracts/Interfaces/IStartupTask.cs
+++ backend/ZiziBot.Contracts/Interfaces/IStartupTask.cs
@@ -5,7 +5,5 @@ namespace ZiziBot.Contracts.Interfaces;
 [UsedImplicitly(ImplicitUseTargetFlags.WithInheritors)]
 public interface IStartupTask
 {
-    public bool SkipAwait { get; set; }
-
     Task ExecuteAsync();
 }
