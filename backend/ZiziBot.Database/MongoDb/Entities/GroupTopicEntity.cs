16674db54fdf57140078ddfcaeba6a2102ec1c50
Azhe Kun <root@corp.azhe.my.id>
2025-06-26 20:58:03 +0700

    nullable thread

--- backend/ZiziBot.Database/MongoDb/Entities/GroupTopicEntity.cs
+++ backend/ZiziBot.Database/MongoDb/Entities/GroupTopicEntity.cs
@@ -6,8 +6,8 @@ namespace ZiziBot.Database.MongoDb.Entities;
 public class GroupTopicEntity : EntityBase
 {
     public long ChatId { get; set; }
-    public int ThreadId { get; set; }
-    public string ThreadName { get; set; }
+    public int? ThreadId { get; set; }
+    public string? ThreadName { get; set; }
     public int IconColor { get; set; }
     public long IconCustomEmojiId { get; set; }
 }
