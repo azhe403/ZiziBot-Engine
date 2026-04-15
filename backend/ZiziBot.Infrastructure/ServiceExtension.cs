ea2bcd7217712f8a5f57a4827d7701a33fec480d
Azhe Kun <root@corp.azhe.my.id>
2026-04-16 05:46:47 +0700

    update service scoped call

--- backend/ZiziBot.Infrastructure/ServiceExtension.cs
+++ backend/ZiziBot.Infrastructure/ServiceExtension.cs
@@ -98,6 +98,7 @@ public static class ServiceExtension
             .WithScopedLifetime());
 
         services.AddScoped<ServiceFacade>();
+        services.AddSingleton<IServiceScopedUtil, ServiceScopedUtil>();
 
         return services;
     }
